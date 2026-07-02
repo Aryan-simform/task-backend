ddcec155a96d690f7c560a92785631a1342e4073
aryan-simform <aryan.mahida@simformsolutions.com>
2026-07-02 21:51:19 +0530

    chore: first commit adds husky and swagger

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('My API')
+    .setDescription('API description')
+    .setVersion('0.0.0')
+    .addTag('API')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory());
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
